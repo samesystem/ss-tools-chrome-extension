f2a502671bd0f26fea5690d28d1844a4b739b2c6
Povilas Jurcys <po.jurcys@gmail.com>
2025-09-03 11:04:10 +0300

    Make copy branch button less ego-centered

--- content.js
+++ content.js
@@ -7,8 +7,8 @@
              width="18" height="18"
              viewBox="0 0 32 32" enable-background="new 0 0 32 32" xml:space="preserve"
         >
-            <rect x="13" y="9" fill="none" stroke="#000000" stroke-width="2" stroke-miterlimit="10" width="14" height="18"/>
-            <polyline fill="none" stroke="#000000" stroke-width="2" stroke-miterlimit="10" points="11,23 5,23 5,5 19,5 19,7 "/>
+            <rect x="13" y="9" fill="none" stroke="#26a69a" stroke-width="2" stroke-miterlimit="10" width="14" height="18"/>
+            <polyline fill="none" stroke="#26a69a" stroke-width="2" stroke-miterlimit="10" points="11,23 5,23 5,5 19,5 19,7 "/>
         </svg>
     `
     // Function to inject Bootstrap CSS into the page
@@ -284,13 +284,13 @@
         // Create a copy link and insert at the end of the branch div
         const copyLink = document.createElement('a');
         copyLink.innerHTML = COPY_SVG;
-        copyLink.className = "btn btn-sm btn-outline-primary ms-1";
+        copyLink.className = "btn btn-sm btn-link";
         copyLink.addEventListener('click', function(e) {
             e.preventDefault();
             writeToClipboard(branchName);
             copyLink.innerHTML = 'Copied!';
-            setTimeout(() => {
-                copyLink.innerHTML = COPY_SVG;
+             setTimeout(() => {
+                 copyLink.innerHTML = COPY_SVG;
             }, 1500);
         });
         branchDiv.appendChild(copyLink);
